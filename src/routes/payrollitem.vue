babcee296b308a167f1d37f6e9f9a05bb3fab137
Muyinda Rogers <moverr@gmail.com>
2024-04-04 09:36:21 +0300

    :Debugging :

--- src/routes/payrollitem.vue
+++ src/routes/payrollitem.vue
@@ -1,40 +1,74 @@
 <template>
 
     <div>
-        <form action="" method="post">
+        <div>
+            <v-header :icon-link="`/${currentProjectKey}/collections`" />
+            <v-loader area="content" />
+        </div>
 
-            <div class="section drop-down">
-                <label for=""> Branches </label>
-                <select  v-model="selectedBranch" @change='fetchDepartments'>
-                    <option value="">Select Option </option>
-                    <option v-for="branch in branches" :key="branch.id" :value="branch.id">
-                        {{ branch.name }}</option>
-                </select>
-
-            </div>
-
-            <div class="section drop-down">
-                <label for="">Deparment</label>
-                <select  v-model="selectedDepartment"  >
-                    <option value="">Select Option </option>
-                    <option v-for="department in departments" :key="department.id" :value="department.id">
-                        {{ department.name }}</option>
-                </select>
-
-            </div>
-
-            <div class="section drop-down">
-                Employee
-                <select>
-                    <option disabled>Select Option </option>
-                    <option>Moves</option>
-                </select>
-
-            </div>
+        <v-header :breadcrumb="breadcrumb" :info-toggle="!newItem && !batch && !activityDetail" :icon-link="iconLink"
+            :icon="singleItem ? collectionInfo.icon || 'box' : 'arrow_back'" item-detail
+            :settings="collection === 'directus_webhooks'">
+            <template slot="title">
+                <span v-tooltip="statusName" class="status-indicator"
+                    :style="{ backgroundColor: `var(--${statusColor})` }" />
+            </template>
+            <template slot="buttons">
 
 
-        </form>
+                <v-header-button :loading="saving" :label="$t('save')" :options="saveOptions" icon="check"
+                    icon-color="button-primary-text-color" background-color="button-primary-background-color"
+                    hover-color="button-primary-background-color-hover"
+                    @click="singleItem ? save('stay') : save('leave')" @input="save" />
+            </template>
+        </v-header>
 
+        <div class="inline-form " style="margin-top: 10px; margin-left:40px;">
+            <form action="" method="post">
+
+                <div class="  drop-down">
+                    <label for=""> Branches </label>
+                    <select v-model="selectedBranch" @change='fetchDepartments'>
+                        <option value="">Select Option </option>
+                        <option v-for="branch in branches" :key="branch.id" :value="branch.id">
+                            {{ branch.name }}</option>
+                    </select>
+
+                </div>
+
+                <div class="  drop-down">
+                    <label for="">Deparment</label>
+                    <select v-model="selectedDepartment" @change='fetchEmployees'>
+                        <option value="">Select Option </option>
+                        <option v-for="department in departments" :key="department.id" :value="department.id">
+                            {{ department.name }}</option>
+                    </select>
+
+                </div>
+
+                <div class="  drop-down">
+                    <label for=""> Employee </label>
+                    <select v-model="selectedEmployee" @change="displaySelectedEmployee">
+                        <option value="">Select Option </option>
+                        <option v-for="employee in employees" :key="employee.id" :value="employee.id">
+                            {{ employee.first_name }}
+                            {{ employee.last_name }}
+                        </option>
+                    </select>
+
+                </div>
+
+
+            </form>
+        </div>
+
+        <div v-if="employeeDetail !== '' && employeeDetail !== undefined" class="inline-form "
+            style="margin-top: 10px; margin-left:40px;">
+            <label for=""><strong>Name:</strong> {{ employeeDetail.first_name }} {{ employeeDetail.last_name }} </label>
+            <label for="">Job Status: {{ employeeDetail.job_status }} </label>
+            <label for="">Gross Amount: {{ employeeDetail.gross_salary }} </label>
+
+        </div>
     </div>
 
 </template>
@@ -63,11 +97,38 @@ export default {
         return {
             selectedBranch: "",
             selectedDepartment: "",
-            branches: [], departments: []
+            selectedEmployee: "",
+            employeeDetail: "",
+            branches: [], departments: [], employees: []
         };
     },
 
     methods: {
+
+
+        displaySelectedEmployee() {
+            let br = this.employees;
+            let bingo = br.find(x => x.id == this.selectedEmployee);
+            this.employeeDetail = bingo;
+            console.log(this.employeeDetail)
+
+        }
+        ,
+        fetchEmployees() {
+            console.log("Beneere")
+            this.$api.getItems("employees", {
+                fields: '*.*'
+
+            })
+                .then(res => res.data)
+                .then(data => {
+                    console.log(data);
+
+                    this.employees = data;
+                })
+
+
+        },
 
 
         fetchDepartments() {
@@ -87,7 +148,7 @@ export default {
         },
 
 
-         fetchBranches() {
+        fetchBranches() {
             console.log("Beneere")
             this.$api.getItems("branches", {
                 fields: '*.*'
@@ -108,6 +169,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.inline-form {
+
+    width: 1000%;
+    float: left;
+    padding: 5px;
+    padding-left: 15px;
+}
+
+.inline-form .drop-down {
+    float: left;
+}
+
+.inline-form select {
+    padding: 10px;
+    font-size: 14px;
+    width: 300px !important;
+    overflow: hidden;
+    margin-left: 4px;
+}
+
 .edit {
     padding: var(--page-padding-top) var(--page-padding) var(--page-padding-bottom);
 }
